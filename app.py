4d2b21e0573cfb6232523b3da7d3e505a90daf57
junepan46 <junepan@junes-Air.home>
2019-01-07 01:35:41 -0500

    added plot

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template, request, redirect
-
+from classes.stocker import Stocker
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return render_template('index.html')
 
-@app.route('/about')
-def about():
-  return render_template('about.html')
+@app.route('/graph')
+def graph():
+  s = Stocker(ticker='MSFT')
+  data=s.plot_stock(start_date=None, end_date=None, stats=['Close'], plot_type='basic')
+  return render_template('graph.html',graph_data=data)
 
 if __name__ == '__main__':
   app.run(port=33507)
